3d67df7ce432a724656b249609955ff45a1c7119
farhankisnanda <kingkiss2901@gmail.com>
2020-10-08 05:28:19 +0700

    Menyelesaikan Latihan For Javascript

---
+++ 20-LatihanPengulanganFor.js
@@ -0,0 +1,11 @@
+var noAngkot = 1;
+var angkotBeroperasi =6;
+var jmlAngkot = 10;
+
+while(noAngkot<=angkotBeroperasi){
+    console.log('Angkot No.'+noAngkot+' '+'beroperasi dengan baik.');
+    noAngkot++;
+}
+for(noAngkot=angkotBeroperasi+1;noAngkot<=jmlAngkot;noAngkot++){
+    console.log('Angkot No.'+noAngkot+' '+'sedang tidak beroperasi.');
+}
