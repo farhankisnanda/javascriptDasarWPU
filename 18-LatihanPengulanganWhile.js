29c0eeba010c9640698f475a43fe2ae26d26bde5
farhankisnanda <kingkiss2901@gmail.com>
2020-10-08 02:04:10 +0700

    Menyelesaikan Latihan WhileAngkot

--- 18-LatihanPengulanganWhile.js
+++ 18-LatihanPengulanganWhile.js
@@ -1,5 +1,17 @@
+// Membuat Hello World Sebanyak 5x
 var nilaiAwal = 1
 while (nilaiAwal <= 5) {
   console.log('Hello World!'+' '+nilaiAwal+"x");
   nilaiAwal++;
 }
+// Akhir Membuat Hello World Sebanyak 5x
+
+console.log ("")
+
+// Latihan Angkot
+var a = 1;
+while (a <= 10) {
+    console.log('Angkot No.'+" "+a+" "+"beroperasi dengan baik.");
+    a++;
+}
+// Akhir Latihan Angkot
