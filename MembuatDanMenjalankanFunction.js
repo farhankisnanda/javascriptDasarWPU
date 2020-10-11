e39c08cb357933a8f034988a06a40ee3a49186c7
farhankisnanda <kingkiss2901@gmail.com>
2020-10-11 16:02:53 +0700

    Menyelesaikan Latihan Membuat dan Menjalankan Function

---
+++ MembuatDanMenjalankanFunction.js
@@ -0,0 +1,30 @@
+// Jawaban Gua
+// function volumeKubus (a){
+//     hasil = a*a*a;
+//     return hasil;
+// }
+// b = volumeKubus (7);
+// c = volumeKubus (4);
+
+// function jumlahVolume (b,c){
+//     return b+c;
+// }
+// jumlah = jumlahVolume(b,c)
+// console.log(jumlah)
+// Akhir Jawaban Gua
+
+// Jawaban WPU
+function jumlahVolumeDuaKubus (a,b){
+    var volumeB;
+    var volumeA;
+    var jumlah;
+
+    volumeA = a * a * a;
+    volumeB = b * b * b;
+    jumlah = volumeA + volumeB;
+
+    return jumlah;
+}
+console.log (jumlahVolumeDuaKubus(2,3));
+console.log (jumlahVolumeDuaKubus(3,9));
+// Akhir Jawaban WPu
